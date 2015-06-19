dba72fcfe7265232e1067354f0800a832b4e5cc5
acracia <acracia@cryptodrunks.net>
2015-06-19 18:10:23 +0200

    setting up the admin

--- rebost/tenda/admin.py
+++ rebost/tenda/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.conf import settings
+from rebost.tenda.models import Product
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_filter = ['available']
+    search_fields = ['name', 'producer', 'price']
+
+admin.site.register(Product, ProductAdmin)
+
