3e4ce8775698bc46d00c758d829214813ca47fdf
efkin <efkin@cooperativa.cat>
2015-06-19 12:16:17 +0200

    pep8 fixes

--- rebost/tenda/models.py
+++ rebost/tenda/models.py
@@ -6,12 +6,14 @@ from django.utils.translation import ugettext as _
 
 class Ecoxarxa(models.Model):
     name = models.CharField(max_length=100, help_text=_('Name'))
-    url = URLField(help_text=_('web page'))
+    url = models.URLField(help_text=_('web page'))
+
 
 class Rebost(models.Model):
     name = models.CharField(max_length=100, help_text=_('Name'))
     ecoxarxa = models.ForeignKey(Ecoxarxa, related_name="rebosts")
     group = models.OneToOneField(settings.AUTH_GROUP_MODEL, unique=True)
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100, help_text=_('Name'))
@@ -24,12 +26,16 @@ class Product(models.Model):
     producer = models.ForeignKey(Prosumer, related_name="products",
                                  related_query_name="products")
     stock = models.IntegerField()
+
     def available():
-            return ( self.stock >=1 )
+            return (self.stock >=1 )
+
+
 class Prosumer(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
     ces_account = models.Charfield(help_text=_('User account in IntegralCES'))
-    
+
+
 class Exchange(models.Model):
     rebost = models.ForeignKey(Rebost, related_name="exchange")
     date = models.DateField()
