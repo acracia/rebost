b7117a9b815bb4c815e9708b521ca3d8b2169400
acracia <acracia@cryptodrunks.net>
2015-06-20 00:30:59 +0200

    more models

--- rebost/tenda/models.py
+++ rebost/tenda/models.py
@@ -36,14 +36,28 @@ class Prosumer(models.Model):
     ces_account = models.Charfield(help_text=_('User account in IntegralCES'))
 
 
-class Exchange(models.Model):
+class ExchangeDay(models.Model):
+    '''
+    One day for exchanging goods in the exchange group
+    '''
     rebost = models.ForeignKey(Rebost, related_name="exchange")
     date = models.DateField()
+    closing_date = models.IntegerField(
+        help_text=_('How many days in aadvance the comanda closes'))
+
 
 class Comanda(models.Model):
+    '''
+    A list of products to be delivered to the user on an ExchangeDay
+    '''
     user = models.ForeignKey(Prosumer, related_name="comandes")
     products = models.ManyToManyField(Product, related_name="comandas")
     exchange = models.ForeignKey(Exchange, related_name="comandas")
     #    status = TODO. must be something like 'pending, delivered, charged...'
 
-
+class Payment(models.Model):
+    '''
+    Payment process for one Comanda
+    '''
+    comanda = models.ForeignKey(Comanda, related_name="payment")
+    url = models.URLField(help_text=_('web page'))
