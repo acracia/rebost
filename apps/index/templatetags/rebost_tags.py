dac53cc9375d3289012742eaca72fc1647f8cfd2
efkin <efkin@cooperativa.cat>
2015-06-20 22:09:43 +0200

    implemented filters for user groups

--- apps/index/templatetags/rebost_tags.py
+++ apps/index/templatetags/rebost_tags.py
@@ -1,5 +1,6 @@
 from django import template
-
+from django.contrib.auth.models import User
+from apps.tenda import auth_groups
 
 register = template.Library()
 
@@ -17,3 +18,32 @@ def login_form_field(field, hide_label=False):
 @register.inclusion_tag('tags/non_field_errors.html')
 def non_field_errors(form):
     return {'form': form}
+
+
+# Auth Membership Check
+def _is_member(user, group_name):
+    """
+    Returns True if the given User object is a member of a group with the given name.
+    Please choose the names from the reobst.settings.auth_groups constants.
+    :type group_name: basestring
+    :type user: django.contrib.auth.User
+    """
+    if isinstance(user, User):
+        return user.groups.filter(name=group_name).exists()
+
+    return False
+
+
+@register.filter(name='is_super_admin')
+def is_super_admin(u):
+    return _is_member(u, auth_groups.SUPER_ADMIN)
+
+
+@register.filter(name='is_ecoxarxa_admin')
+def is_ecoxarxa_admin(u):
+    return _is_member(u, auth_groups.XARXA_ADMIN)
+
+
+@register.filter(name='is_rebost_admin')
+def is_rebost_admin(u):
+    return _is_member(u, auth_groups.REBOST_ADMIN)
---
+++ apps/tenda/auth_groups.py
@@ -0,0 +1,13 @@
+# Try to use names consistently according to
+# the permissions that you want to grant
+
+# This group will have CRUD privileges around prosumers
+REBOST_ADMIN = "Rebost Admin"
+
+# This group will have CRUD privileges around rebosts
+XARXA_ADMIN = "Ecoxarxa Admin"
+
+# This group will have CRUD privileges around eco-networks
+SUPER_ADMIN = "Super Admin"
+
+# The remaining users are obviuosly prosumers
